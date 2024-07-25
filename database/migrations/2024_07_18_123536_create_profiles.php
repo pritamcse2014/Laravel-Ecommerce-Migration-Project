fd1c0c968d60bfbf4de233e73eb16b8aa3f18f09
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-07-25 08:40:32 +0600

    profiles-table-complete-relation-to-users-table

--- database/migrations/2024_07_18_123536_create_profiles.php
+++ database/migrations/2024_07_18_123536_create_profiles.php
@@ -13,7 +13,18 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('firstName', 50);
+            $table->string('lastName', 50);
+            $table->string('mobile', 50);
+            $table->string('city', 50);
+            $table->string('shippingAddress', 1000);
+            $table->string('email', 50)->unique();
+            // Relationship
+            $table->foreign('email')->references('email')->on('users')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
