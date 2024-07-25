45e19ca3400b3f2d0eead59dfe7f721b141d21a0
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-07-25 20:11:55 +0600

    product-sliders-table-complete-foreign-key-and-relation-to-products-table

--- database/migrations/2024_07_18_123724_create_product_sliders.php
+++ database/migrations/2024_07_18_123724_create_product_sliders.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('product_sliders', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title', 200);
+            $table->string('short_des', 500);
+            $table->string('image', 200);
+            // Foreign Key (product id)
+            $table->unsignedBigInteger('product_id')->unique();
+            // Products Relationship
+            $table->foreign('product_id')->references('id')->on('products')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
